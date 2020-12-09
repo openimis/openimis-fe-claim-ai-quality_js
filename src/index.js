eb0c1ba9e73dccfc453925eebbbc60990c83095c
Damian Borowiecki <dborowiecki@soldevelo.com>
2020-12-09 17:04:33 +0100

    OAI-19: Added medical item filtering for claim search

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import messages_en from "./translations/en.json";
+import ClaimFilterByInsureeMedicalItem from "./components/ClaimFilterMedicalItem";
 
 const DEFAULT_CONFIG = {
   "translations": [{ key: "en", messages: messages_en }],
+  "claim.Filter": [ClaimFilterByInsureeMedicalItem]
 }
 
 export const ClaimAIQualityModule = (cfg) => {
